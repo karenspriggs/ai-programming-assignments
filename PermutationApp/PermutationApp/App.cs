dcfcd67c7e0c1cfd764a329b5de9c779ecb4d555
Karen Spriggs <k.spriggs01@gmail.com>
2023-03-08 09:22:02 -0600

    Update App.cs

--- PermutationApp/PermutationApp/App.cs
+++ PermutationApp/PermutationApp/App.cs
@@ -17,40 +17,57 @@ namespace PermutationApp
 
         public void Run()
         {
-
+            char[] inputArray = GetElementInput();
+            PrintPermutations(inputArray);
         }
 
-        void TakeInput()
+        char[] GetElementInput()
         {
+            Console.WriteLine("Please enter 5 characters and then hit enter to submit:");
+            char[] inputArray = Console.ReadLine().ToCharArray();
 
-        }
-
-        string GetElementInput()
-        {
-            Console.WriteLine("Please enter the next element of the list");
-            string choice = Console.ReadLine();
-
-            if (choice == "")
+            if (inputArray.Length != 5)
             {
-                Console.WriteLine("You did not enter a character, try again");
+                Console.WriteLine("You did not enter 5 characters, please try again.");
                 GetElementInput();
             }
 
-            Console.WriteLine($"The next element you entered was {choice.Substring(0, 1)}");
-
-            return choice.Substring(0, 1);
+            return inputArray;
         }
 
-        void MakePermutations()
+        void PrintPermutations(char[] symbolArray)
         {
+            Console.WriteLine("Permutation list:");
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"5P{i}:");
+                Permute(symbolArray, 0, i, new List<char>());
+                Console.WriteLine($" Amount of permutations: {FindPermutationCount(symbolArray.Length, i)}");
+            }
 
+            Console.WriteLine("Ordered partition list:");
+
+            Console.WriteLine("Combination list:");
+            for(int i = 1; i <=5; i++)
+            {
+                Console.WriteLine($"5C{i}:");
+                Console.WriteLine($" Amount of combinations: {FindCombinationCount(symbolArray.Length, i)}");
+            }
         }
 
-        void MakePermutatiosnOfLength(int length)
+        void Permute(char[] symbolArray, int startingIndex, int length, List<char> results)
         {
-            int count = 0;
-
-
+            if (length == 0)
+            {
+                Console.WriteLine(results.ToString());
+            }else
+            {
+                for(int i = startingIndex; i <= symbolArray.Length - length; i++)
+                {
+                    results[results.Count - length] = symbolArray[i];
+                    Permute(symbolArray, i+1, length-1, results);
+                }
+            }
         }
 
         void MakePartitions()
@@ -61,6 +78,68 @@ namespace PermutationApp
         void MakeCombinations()
         {
 
+        }
+
+        int FindFactorial(int number)
+        {
+            int result = number;
+
+            // Factorial of 0 is 1
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            for (int i = number - 1; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        // Amount of permutations is N! / (N-R)!
+        int FindPermutationCount(int number, int taken)
+        {
+            return FindFactorial(number) / FindFactorial(number - taken);
+        }
+
+        // Amount of partitions depends on the amount of repeated elements
+        int FindPartitionCount(char[] symbolArray, int number)
+        {
+            int currentDenominator = 1;
+
+            // Storing the amount of each char found in dictionary
+            Dictionary<char, int> foundChars = new Dictionary<char, int>();
+
+            // Go through the array to add the elements to the dictionary
+            foreach(Char c in symbolArray)
+            {
+                // If it was already found prior, add one to the count
+                if (foundChars.ContainsKey(c))
+                {
+                    foundChars[c] += 1;
+                }else
+                {
+                    foundChars.Add(c, 1);
+                }
+            }
+
+            // The denominator is the product of the factorial of the count
+            // of each of the elements
+            foreach(KeyValuePair<char, int> pair in foundChars)
+            {
+                currentDenominator *= FindFactorial(pair.Value);
+            }
+
+            return FindFactorial(number)/currentDenominator;
+        }
+
+        // Amount of combinations is (N! / (N-R)!) / R!
+        // Which is also the amount of permutations divided by R!
+        int FindCombinationCount(int number, int taken)
+        {
+            return FindPermutationCount(number,taken)/FindFactorial(taken);
         }
     }
 }
